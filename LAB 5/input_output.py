952d2b752a4e0643f3477402eb8f3e1635004487
aziret2003a-wq <aziret2003a@gmail.com>
2026-02-09 15:04:24 +0600

    second  try

---
+++ LAB 5/input_output.py
@@ -0,0 +1,25 @@
+name = input("Введите имя: ")
+age = input("Введите возраст: ")
+
+print("Меня зовут", name, "и мне", age, "лет.")
+
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+c = float(input("Введите третье число: "))
+
+sum_numbers = a + b + c
+average = sum_numbers / 3
+product = a * b * c
+
+print("Сумма:", sum_numbers)
+print("Среднее:", average)
+print("Произведение:", product)
+
+# Это программа считает площадь круга
+
+PI = 3.14159  # Константа числа π
+radius = 5   # Радиус круга
+
+area = PI * radius ** 2
+print(area)
+
