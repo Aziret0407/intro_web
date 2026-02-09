d6ca0195426b18233738a5e302b20a47273e79f8
aziret2003a-wq <aziret2003a@gmail.com>
2026-02-09 15:02:05 +0600

    first try

---
+++ LAB 5/variables.py
@@ -0,0 +1,31 @@
+name = "Ivan"
+print(name)
+
+a = 10
+b = 5
+
+print("Сложение:", a + b)
+print("Вычитание:", a - b)
+print("Умножение:", a * b)
+print("Деление:", a / b)
+
+x = 10
+print(type(x))
+
+x = "hello"
+print(type(x))
+
+a = 3
+b = 7
+
+a, b = b, a
+
+print("a =", a)
+print("b =", b)
+
+PI = 3.14159
+radius = 5
+
+area = PI * radius ** 2
+print("Площадь круга:", area)
+
